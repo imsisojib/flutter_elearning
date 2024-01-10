5fc18015c00294ba5d6e723f82d81945e094c830
Tanvir Anwar Rafi <tanvir.anwar2@gmail.com>
2024-01-10 17:05:01 +0600

    translation added for auth UIs

---
+++ lib/src/features/language/data/language_key.dart
@@ -0,0 +1,23 @@
+class LanguageKey{
+  static const String enterYourPhoneNumberToRegister = "enterYourPhoneNumberToRegister";
+  static const String afterEnteringThePhoneNumber6digits = "afterEnteringThePhoneNumber6digits";
+  static const String otpCodeWillBeSend = "otpCodeWillBeSend";
+  static const String phoneNumber = "phoneNumber";
+  static const String continueText = "continue";
+  static const String enterTheVerificationCode = "enterTheVerificationCode";
+  static const String otp6NumbersWillBeSentForVerification = "otp6NumbersWillBeSentForVerification";
+  static const String resendOTPCode = "resendOTPCode";
+  static const String successful = "successful";
+  static const String verified = "verified";
+  static const String congratulationsPhoneNumberVerified = "congratulationsPhoneNumberVerified";
+  static const String lastStepFor = "lastStepFor";
+  static const String registration = "registration";
+  static const String firstName = "firstName";
+  static const String lastName = "lastName";
+  static const String requiredInBraked = "required_in_braked";
+  static const String optionalInBraked = "optional_in_braked";
+  static const String email = "email";
+  static const String registerAs = "registerAs";
+  static const String registrationIsSuccessful = "registrationIsSuccessful";
+  static const String select = "select";
+}
